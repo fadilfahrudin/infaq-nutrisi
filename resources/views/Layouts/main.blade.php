c6963ecc26a4deb6c10b7e1e595bf95adcdabc40
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-02-16 11:18:23 +0700

    new featurs

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -7,6 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet">
+	  <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 
     <title>INFAQ NUTRISI | {{ $title }}</title>
@@ -28,6 +30,39 @@
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" ></script>
     <script src="/js/script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+    </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+    </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js">
+    </script> 
+    <script>
+           $('.team-slider').owlCarousel({
+               loop: true,            
+               nav: false,
+               autoplay: true,
+               autoplayTimeout: 5000,
+               smartSpeed: 450,
+               margin: 20,
+               responsive: {
+                   0: {
+                       items: 1
+                   },
+                   768: {
+                       items: 2
+                   },
+                   991: {
+                       items: 3
+                   },
+                   1200: {
+                       items: 3
+                   },
+                   1920: {
+                       items: 3
+                   }
+               }
+           });
+    </script>
 
   </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,71 +1,235 @@
 @extends('layouts.main')
 
-@include('partials.navbar')
+@include('partials.navbar') {{-- Navbar --}}
 @section('content')
-<section class="main">
-{{-- Header Hero --}}
-    <header class="hero">
-            <div class="text-heading" >
-                <img src="img/name-Landscape.png"  width="500px" alt="Gerakan Infaq Nutrisi">
-                <p class=" text-p">Semua bisa bergerak, Semua bisa <br> berinfaq, Semua mampu bantu sesama</p>
-                <a href="/berdonasi" target="_blank"><button class="btn-donation">Donasi Sekarang</button></a>
+<section class="main mt-5 ">
+{{-- Header --}}
+    <header class="header px-3 d-flex">
+        <div class="row align-items-center">
+            <div class="col col-md-6">
+                <img src="img/name-Landscape.png" class="w-100"   alt="">
+                <h3 class="text-center mb-5">Semua bisa bergerak, Semua bisa <br> berinfaq, Semua mampu bantu sesama</h3>
+                <a href="/berdonasi" class="btn btn-donasi d-grid col-6 mx-auto">Donasi Sekarang</a>
             </div>
-
-             <div class="ill-hero" >
-                   <img src="img/hero.png" alt="Gerakan Infaq nutrisi">
+            <div class="col-md-6">
+                <img src="img/hero.png" class="hero w-100" alt="Gerakan Infaq nutrisi">
             </div>
+        </div>
     </header>
 
-    {{-- End --}}
+    {{-- End Header --}}
 
-    <section class="vektor-il">
-        <img src="img/Vector-1.png" alt="">
-    </section>
+    {{-- About --}}
+    <section class="about">
 
-    {{-- Main Content --}}
-    <section class="main-content">
+        <div class="report py-5">
+            <div class="row gap-1  px-5 py-3 mx-0">
+                <a href="#" class="btn-report col col-sm">
 
-       
+                    <div class="item1">Donasi Terhimpun</div>
+                    <div class="item2">Rp.</div>
+                    <div class="item3">100.000.000</div>  
+                    <div class="item4"><img src="img/ic-donation-collage.svg" alt=""></div>
+                    
+                </a>
 
-        {{-- Report --}}
-        <div class="report">
+                <a href="#" class="btn-report col col-sm">
 
-            <a href="#" class="item-report">
-                <div class="item1">Donasi Terhimpun</div>
-                <div class="item2">Rp.</div>
-                <div class="item3">100.000.000</div>  
-                <div class="item4"><img src="img/ic-donation-collage.svg" alt=""></div>
-             </a>
+                    <div class="item1">Donasi Terhimpun</div>
+                    <div class="item2">Rp.</div>
+                    <div class="item3">100.000.000</div>  
+                    <div class="item4"><img src="img/ic-donation-collage.svg" alt=""></div>
+                    
+                </a>
 
-            <a href="#" class="item-report">
-                <div class="item1">Donasi Tersalurkan</div>
-                <div class="item2">Rp.</div>
-                <div class="item3">100.000.000</div>  
-                <div class="item4"><img src="img/ic-donation-distributed.svg" alt=""></div>
-            </a>
+                <a href="#" class="btn-report col col-sm">
 
-            <a href="#" class="item-report">
-                <div class="item1">Paket Tersalurkan</div>
-                <div class="item2">Rp.</div>
-                <div class="item3">100.000.000</div>  
-                <div class="item4"><img src="img/ic-package-distributed.svg" alt=""></div>
-            </a>
+                    <div class="item1">Donasi Terhimpun</div>
+                    <div class="item2">Rp.</div>
+                    <div class="item3">100.000.000</div>  
+                    <div class="item4"><img src="img/ic-donation-collage.svg" alt=""></div>
+                    
+                </a>
 
-            <a href="#" class="item-report">
-                <div class="item1">Penerima Manfaat</div>
-                <div class="item2">Rp.</div>
-                <div class="item3">20.000</div>  
-                <div class="item4"><img src="img/ic-recipients.svg" alt=""></div>
-            </a>
+                <a href="#" class="btn-report col col-sm">
+
+                    <div class="item1">Donasi Terhimpun</div>
+                    <div class="item2">Rp.</div>
+                    <div class="item3">100.000.000</div>  
+                    <div class="item4"><img src="img/ic-donation-collage.svg" alt=""></div>
+                    
+                </a>
+            </div>
         </div>
-        {{-- end Report --}}
 
-    <div class="vector2-il">
-         <img src="img/Vector-2.svg" alt="">
-     </div>
-        
+        <div class="carousel py-5 px-5 ">
+            <div class="row gap-5">
+                <div class="col-12 col-sm px-3">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <img src="img/1.jpg" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="img/2.jpg" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="img/3.jpg" class="d-block w-100" alt="...">
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+                </div>
+                <div class="about-content col-12 col-md-6 text-center lh-lg px-3">
+                    <h3>Apa itu Gerakan Infaq Nutrisi?</h3>
+                    <p>Gerakan Infaq Nutrisi (GIN) merupakan sebuah gerakan mengajak masyarakat untuk saling bergerak dan membantu sesama. GIN menyalurkan kebutuhan 
+                    pokok atau paket nutrisi yang sehat guna 
+                    memperbaiki nutrisi keluarga.</p>
+                        
+                    <p> Paket nutrisi seperti beras, minyak goreng, gula, susu/madu herbal, sayuran, bumbu dapur, buah, telur/lauk, dan tempe/tahu. Paket nutrisi akan diberikan kepada keluarga dhuafa, panti asuhan, dan pesantren.</p>
+
+                    <a href="/berdonasi" class="btn btn-donasi d-grid col-6 mx-auto">Donasi Sekarang</a>
+                </div>
+            </div>
+        </div>
 
     </section>
+    {{-- End About --}}
+
+    {{--Start Tokoh Pendukung --}}
+    <section class="tokoh-pendukung">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                        <div class="sec-heading text-center">
+                            <h3>Tokoh Pendukung</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-box">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="team-slider owl-carousel">
+                                <div class="single-box text-center">
+                                    <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/bg.jpg"></div>
+                                    <div class="info-area">
+                                        <h4>Person's Name</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                    </div>
+                                </div>
+                                <div class="single-box text-center">
+                                    <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/1.jpg"></div>
+                                    <div class="info-area">
+                                        <h4>Person's Name</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                    </div>
+                                </div>
+                                <div class="single-box text-center">
+                                    <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/1.jpg"></div>
+                                    <div class="info-area">
+                                        <h4>Person's Name</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                    </div>
+                                </div>
+                                <div class="single-box text-center">
+                                    <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/3.jpg"></div>
+                                    <div class="info-area">
+                                        <h4>Person's Name</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                    </div>
+                                </div>
+                                <div class="single-box text-center">
+                                    <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/3.jpg"></div>
+                                    <div class="info-area">
+                                        <h4>Person's Name</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    {{-- End Tokoh Pendukung --}}
+
+    {{-- Start Penerima Manfaat --}}
+    <section class="penerima-manfaat">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                    <div class="sec-heading text-center">
+                        <h3>Penerima Manfaat</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="testimonial-box">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="team-slider owl-carousel">
+                            <div class="single-box text-center">
+                                <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/bg.jpg"></div>
+                                <div class="info-area">
+                                    <h4>Person's Name</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                </div>
+                            </div>
+                            <div class="single-box text-center">
+                                <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/1.jpg"></div>
+                                <div class="info-area">
+                                    <h4>Person's Name</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                </div>
+                            </div>
+                            <div class="single-box text-center">
+                                <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/1.jpg"></div>
+                                <div class="info-area">
+                                    <h4>Person's Name</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                </div>
+                            </div>
+                            <div class="single-box text-center">
+                                <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/3.jpg"></div>
+                                <div class="info-area">
+                                    <h4>Person's Name</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                </div>
+                            </div>
+                            <div class="single-box text-center">
+                                <div class="img-area"><img alt="" class="img-fluid move-animation" src="img/3.jpg"></div>
+                                <div class="info-area">
+                                    <h4>Person's Name</h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ullam.</p><a href="#">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- End Penerima Manfaat --}}
+
+ 
+   @include('partials.footer') {{-- Footer  --}}
+
+</section>
 
     {{-- end main Content --}}
 
