13b526356034e481266af1eaef5f6e80f5d6d31f
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-02-17 15:24:47 +0700

    benerin layouts footer

--- resources/views/Partials/footer.blade.php
+++ resources/views/Partials/footer.blade.php
@@ -1,33 +1,33 @@
 <footer class="footer py-5 px-5">
     <div class="container">
-            <div class="card-footer overflow-hidden px-5 py-4 d-flex align-items-center " style="border-radius: 4vw">
-                <div class="row">
-                    <div class="col-7 col-md-6">
-                            <h3>
-                                Semua Bisa Bergerak, Semua Bisa Berinfaq, Semua Mampu Bantu Sesama
-                            </h3>
-                            <p>
-                                Kunjungi Sosial Media <br> Kami di: 
-                            </p>
-    
-                            <a href="#" class="btn btn-sosmed mb-2" role="button">infaq nutrisi</a>
-                            <a href="#" class="btn btn-sosmed mb-2" role="button">Gerakan Tv</a>
-                    </div>
+        <div class="card-footer overflow-hidden px-4 py-4" style="border-radius: 4vw">
+            <div class="row">
+                <div class="col col-md-6 m-auto">
+                    <h3>
+                        Semua Bisa Bergerak, Semua Bisa Berinfaq, Semua Mampu Bantu Sesama
+                    </h3>
+                    <p>
+                        Kunjungi Sosial Media <br> Kami di:
+                    </p>
 
-                    <div class="col col-md-6">
-                        <img class="screen-ig position-absolute" src="img/screen-ig.svg" alt="">
-                        <img class="screen-yt position-absolute" src="img/screen-yt.svg" alt="">
-                    </div>
+                    <a href="#" class="btn btn-sosmed mb-2" role="button">infaq nutrisi</a>
+                    <a href="#" class="btn btn-sosmed mb-2" role="button">Gerakan Tv</a>
+                </div>
+
+                <div class="col col-md-6 d-flex">
+                    <img class="screen-ig" src="img/screen-ig.svg" alt="">
+                    <img class="screen-yt " src="img/screen-yt.svg" alt="">
                 </div>
             </div>
+        </div>
 
         <div class="d-block py-5">
             <div class="row">
                 <div class="col">
                     <h4 align="right">Alamat</h4>
-                    <p  align="right">Bintaro Trade Center D 2 No 8 Bintaro<br>Sektor VII Kode Pos 15224</p>
+                    <p align="right">Bintaro Trade Center D 2 No 8 Bintaro<br>Sektor VII Kode Pos 15224</p>
                 </div>
-                    <div class="col">
+                <div class="col">
                     <h4>Hubungi Kami</h4>
                     <p><b>Email : </b>Sektor VII Kode Pos 15224</p>
                 </div>
