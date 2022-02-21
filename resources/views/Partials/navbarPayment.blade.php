609d753af2608dcf99b4a5e14747dea580ed5503
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-02-21 18:40:21 +0700

    Create Navbar for payment

---
+++ resources/views/Partials/navbarPayment.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-light bg-white fixed-top py-1">
+    <div class="container">
+        <a class="navbar-brand mx-auto" href="/">
+            <img src="img/Logo-Landscape.png" alt="" width="120">
+        </a>
+    </div>
+</nav>
