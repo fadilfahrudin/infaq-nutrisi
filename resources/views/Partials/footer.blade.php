ee0ef48cc0ddb4ae3592a27f29d9ebe196c9d6b6
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-03-16 21:36:12 +0700

    merapihkan section tokoh pendukung dan p.manfaat

--- resources/views/Partials/footer.blade.php
+++ resources/views/Partials/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="container row row-cols-5 py-5">
-    <div class="col-md col-12 px-5">
+<footer class="container m-auto row row-cols-5 p-5">
+    <div class="col-md col-12 me-3">
         <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
             <img src="/img/Logo-Landscape.png" width="100" alt="">
         </a>
@@ -8,11 +8,7 @@
         </p>
     </div>
 
-    <div class="col-md-1 col">
-
-    </div>
-
-    <div class="col-md-2 col">
+    <div class="col-md-2 col-4 pt-5">
         <h5>Tentang</h5>
         <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Tentang Kami</a></li>
@@ -20,7 +16,7 @@
         </ul>
     </div>
 
-    <div class="col-md-2 col">
+    <div class="col-md-2 col-4 pt-5">
         <h5>Kategori</h5>
         <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Paket nutrisi</a></li>
@@ -28,7 +24,7 @@
         </ul>
     </div>
 
-    <div class="col-md-2 col">
+    <div class="col-md-2 col pt-5">
         <h5>Pusat Bantuan</h5>
         <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQ</a></li>
