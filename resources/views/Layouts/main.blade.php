cb7506342294ef6cb65c86ab9a84d522d15f247b
fadilfahrudin <fadilfahrudin32@gmail.com>
2022-03-16 21:36:52 +0700

    setup google font

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -13,7 +13,12 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,700;1,200;1,300;1,400;1,700&display=swap"
+        rel="stylesheet">
     <title>INFAQ NUTRISI | @yield('title')</title>
 </head>
 
